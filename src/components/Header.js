91c009fbc616ab753a1a1d39e26d0daffbcbf541
king0120 <king0120@gmail.com>
2017-10-06 12:06:13 -0400

    Added handleItemCurrentlyOnSaleChange

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,14 +14,22 @@ class Header extends Component {
     this.setState({editSaleItem: !this.state.editSaleItem})
   }
 
+  handleItemCurrentlyOnSaleChange = (event) => {
+    this.setState({
+      itemCurrentlyOnSale: event.target.value
+    })
+  }
+
   render () {
     return (
       <div>
         <h1>Hardware Store</h1>
         <p>Currently On Sale: {this.state.itemCurrentlyOnSale}</p>
-        <button onClick={this.toggleEditSaleItem}>Edit Sale Item</button>
+        <button onClick={this.toggleEditSaleItem}>
+          { this.state.editSaleItem ? 'Hide' : 'Edit Sale Item' }
+        </button>
         <div>
-          {this.state.editSaleItem ? <input type="text" placeholder={this.state.itemCurrentlyOnSale} /> : null}
+          {this.state.editSaleItem ? <input type="text" placeholder={this.state.itemCurrentlyOnSale} onChange={this.handleItemCurrentlyOnSaleChange}/> : null}
         </div>
       </div>
     )

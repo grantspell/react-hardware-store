d534c906787ec2078215863c7a13986252e95648
king0120 <king0120@gmail.com>
2017-10-06 14:30:01 -0400

    listing through Products

--- src/components/AdminView.js
+++ src/components/AdminView.js
@@ -1,10 +1,52 @@
 import React, { Component } from 'react'
+import Product from './Product'
 
 class AdminView extends Component {
+  constructor () {
+    super()
+
+    this.state = {
+      productList: [
+        {
+          productName: 'Hammer',
+          description: 'Itsa hammer',
+          price: 12.3
+        },
+        {
+          productName: 'Nail',
+          description: 'Itsa nail',
+          price: 0.12
+        }
+      ]
+    }
+  }
   render () {
     return (
       <div>
-        AdminView
+        <h1>Admin View</h1>
+        <h2>Products</h2>
+        <div>
+          { this.state.productList.map((product, index) => {
+            return (
+              <Product />
+            )
+          }) }
+        </div>
+        <h2>Create A New Product</h2>
+        <form >
+          <div>
+            <input type="text" placeholder="Name"/>
+          </div>
+          <div>
+            <input type="text" placeholder="Price"/>
+          </div>
+          <div>
+            <input type="text" placeholder="Description"/>
+          </div>
+          <div>
+            <button>Create A Product</button>
+          </div>
+        </form>
       </div>
     )
   }
---
+++ src/components/Product.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Product extends Component {
+  render() {
+    return (
+      <div>
+        <h3>{product.productName}</h3>
+        <h4>${product.price}</h4>
+        <p>{product.description}</p>
+      </div>
+    );
+  }
+}
+
+export default Product;

c4e9446cb31d46af327c2908e854492878bb7ba1
king0120 <king0120@gmail.com>
2017-10-06 14:34:00 -0400

    minor spacing edits

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,15 +1,15 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
 
 class Product extends Component {
-  render() {
+  render () {
     return (
       <div>
         <h3>{product.productName}</h3>
         <h4>${product.price}</h4>
         <p>{product.description}</p>
       </div>
-    );
+    )
   }
 }
 
-export default Product;
+export default Product
